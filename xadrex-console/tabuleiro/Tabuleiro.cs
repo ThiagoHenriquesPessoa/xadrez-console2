95865f51f909a60f781d68bb785a143ea99310cd
ThiagoHenriquesPessoa <thiagokxl5@gmail.com>
2021-02-07 17:50:24 -0300

    Começando a colocar peças no tabuleiro

--- xadrex-console/tabuleiro/Tabuleiro.cs
+++ xadrex-console/tabuleiro/Tabuleiro.cs
@@ -14,5 +14,16 @@ namespace tabuleiro
             this.colunas = colunas;
             pecas = new Peca[linhas, colunas];
         }
+
+        public Peca peca(int linha, int coluna)
+        {
+            return pecas[linha, coluna];
+        }
+
+        public void colocarPeca(Peca p, Posicao pos)
+        {
+            pecas[pos.linha, pos.coluna] = p;
+            p.posicao = pos;
+        }
     }
 }
