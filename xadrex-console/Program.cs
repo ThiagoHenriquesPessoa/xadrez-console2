945b841a3897f70a052dafa3fcee7718cb10268a
ThiagoHenriquesPessoa <thiagokxl5@gmail.com>
2021-02-07 14:02:46 -0300

    Iniciando programa de xadrez

---
+++ xadrex-console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xadrex_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hellou Word!");
+            Console.ReadLine();
+        }
+    }
+}
