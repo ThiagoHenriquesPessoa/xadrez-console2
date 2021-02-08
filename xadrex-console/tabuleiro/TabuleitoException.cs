dd3d149218f51656d3deb48450b61ff20bac47aa
ThiagoHenriquesPessoa <thiagokxl5@gmail.com>
2021-02-07 23:37:44 -0300

    Atualização de exception

--- xadrex-console/tabuleiro/TabuleitoException.cs
+++ xadrex-console/tabuleiro/TabuleitoException.cs
@@ -1,24 +1,12 @@
 ﻿using System;
-using System.Runtime.Serialization;
+
 
 namespace tabuleiro
 {
-    [Serializable]
-    internal class TabuleitoException : Exception
+
+    class TabuleitoException : Exception
     {
-        public TabuleitoException()
-        {
-        }
-
-        public TabuleitoException(string message) : base(message)
-        {
-        }
-
-        public TabuleitoException(string message, Exception innerException) : base(message, innerException)
-        {
-        }
-
-        protected TabuleitoException(SerializationInfo info, StreamingContext context) : base(info, context)
+        public TabuleitoException(string msg) : base(msg)
         {
         }
     }
