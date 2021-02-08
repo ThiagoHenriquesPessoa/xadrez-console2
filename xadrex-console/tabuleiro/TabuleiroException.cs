bac30f90f6131a543b58392bf032e01c8c67f7a8
ThiagoHenriquesPessoa <thiagokxl5@gmail.com>
2021-02-07 22:44:31 -0300

    Criei novamente algumas exception

--- xadrex-console/tabuleiro/TabuleiroException.cs
+++ xadrex-console/tabuleiro/TabuleiroException.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace xadrex_console.tabuleiro
+
+namespace tabuleiro
 {
-    class TabuleiroException
+    class TabuleiroException : Exception
     {
+        public TabuleiroException(string msg) : base(msg)
+        {
+        }
     }
 }
---
+++ xadrex-console/tabuleiro/TabuleitoException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace tabuleiro
+{
+    [Serializable]
+    internal class TabuleitoException : Exception
+    {
+        public TabuleitoException()
+        {
+        }
+
+        public TabuleitoException(string message) : base(message)
+        {
+        }
+
+        public TabuleitoException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected TabuleitoException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
